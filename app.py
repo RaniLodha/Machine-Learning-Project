7c11b7c434fefb19ae6c03309bfa20e11a018c7c
RaniLodha <154259899+RaniLodha@users.noreply.github.com>
2024-04-27 12:08:52 +0530

    app.py

--- app.py
+++ app.py
@@ -1,59 +1,70 @@
-import pickle
 import streamlit as st
+import pickle
 import requests
+import pandas as pd
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=e41c6c7d8a32c1ab0cddf8215f135e24&language=en-US'.format(
+            movie_id))
+    data = response.json()
+    if 'poster_path' in data:
+        return "https://image.tmdb.org/t/p/w500" + data['poster_path']
+    else:
+        return None  # Return None if poster_path is not found
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+    if movie in movies_list['title'].values:
+        movie_index = movies_list[movies_list['title'] == movie].index[0]
+        distances = similarity[movie_index]
+        mo_li = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    return recommended_movie_names,recommended_movie_posters
+        recommended_movies = []
+        recommended_movies_posters = []
+        for i in mo_li:
+            movie_id = i[0]
+
+            recommended_movies.append(movies_list.iloc[i[0]].title)
+            # fetch poster from API
+            recommended_movies_posters.append(fetch_poster(movie_id))
+        return recommended_movies, recommended_movies_posters
+    else:
+        st.write(f"Movie '{movie}' not found in the dataset.")
+        return []
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+st.title('Movie Recommender System')
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
+selected_movie_name = st.selectbox('Select a movie', movies_list['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+    for name, poster in zip(names, posters):
+        st.header(name)
+        if poster is not None and poster.startswith('http'):
+            st.image(poster)
+        else:
+            st.write("No poster available for this movie")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+        st.text(names[0])
+        st.image(posters[0])
     with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
+        st.text(names[1])
+        st.image(posters[1])
     with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+        st.text(names[2])
+        st.image(posters[2])
     with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+        st.text(names[3])
+        st.image(posters[3])
     with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
+        st.text(names[4])
+        st.image(posters[4])
 
 
 
